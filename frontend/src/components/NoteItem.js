9d3da7c4d0006ffbce60b77488f6333b14378f64
arshanshaikh97 <arshan@redefcorp.in>
2022-07-03 02:24:29 +0530

    make it better

---
+++ frontend/src/components/NoteItem.js
@@ -0,0 +1,21 @@
+function NoteItem({ note }) {
+  return (
+    <div
+      className='note'
+      style={{
+        backgroundColor: note.isStaff ? 'rgba(0,0,0,0.7)' : '#fff',
+        color: note.isStaff ? '#fff' : '#000',
+      }}
+    >
+      <h4>
+        Note from {note.name ? <span>{note.name} {note.isStaff && "- Tester"}</span> : <span>Unknown</span>}
+      </h4>
+      <p>{note.text}</p>
+      <div className='note-date'>
+        {new Date(note.createdAt).toLocaleString('en-US')}
+      </div>
+    </div>
+  )
+}
+
+export default NoteItem
